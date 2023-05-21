dbf7fb287f7aa6e96bb8fc7f19b31ed94b291d32
Fabianphilip <fabianphilip92@yahoo.com>
2023-05-21 09:42:15 -0700

    Team FATE main header file on simple shell

---
+++ s_shell.h
@@ -0,0 +1,12 @@
+#ifndef S_SHELL_H
+#define S_SHELL_H
+
+#include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+
+void exec(char *agv[], char *argument[]);
+
+#endif
